107e6227ce0cf4424df243a69bb66f812f9bf441
GGSimmions1992 <gary.simmons@colorado.edu>
2018-12-27 14:17:10 -0500

    add the casting

---
+++ VarianceWorld/VarianceWorld.Domain/Models/Casting.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VarianceWorld.Domain.Models
+{
+    public class Casting
+    {
+        public void Implicit()
+        {
+            int x = 10;
+            long y = x;
+            Console.WriteLine(y);
+        }
+        public void Explicit()
+        {
+            short x = 10;
+            byte y = (byte)x;//throws exception if not able to cast (like first)
+            Nullable<byte> z = x as Nullable<byte>;//raises a null value if not able to cast. (like firstOrDefault)
+
+            Console.WriteLine(y);
+        }
+
+        public IEnumerable<byte?> Explicit(short x)
+        {
+            byte y = (byte) x;
+            byte? z = x as byte?;
+
+            //return new { y1=y,z1=z};//Anonymous object
+            return new byte?[]{ y, z };
+        }
+
+        public void Explicit(short x, out byte y, out byte? z)
+        {
+            y = (byte)x;
+            z = x as byte?;
+        }
+
+        public void Explicit(ref short w,ref short x,out byte y, out byte? z)
+        {
+            x = 100;
+            w = x;
+            y = (byte)w;
+            z = w as byte?;//You shouldn't do this. As should only be used as reference.
+        }
+
+        public void Explicit(short w, short x)
+        {
+            x = 100;
+            w = x;
+        }
+
+        public void Converting()
+        {
+            int x = Convert.ToInt16(true);//This will help convert booleans to ones and zeros
+            bool y = Convert.ToBoolean(x);
+            int z = Convert.ToInt32("banana");
+        }
+
+        public int? ConvertingInt(string s)
+        {
+            try
+            {
+                return Convert.ToInt32(s);
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+        
+    }
+}
---
+++ VarianceWorld/VarianceWorld.Tests/CastingExplictTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VarianceWorld.Domain.Models;
+using Xunit;
+using System.Linq;
+
+namespace VarianceWorld.Tests
+{
+    public class CastingExplictTests
+    {
+        [Fact]
+        public void TestExplicit()
+        {
+            //y should be byte
+            //Casting sut1 = new Casting();
+            //var sut2 = sut1.Explicit(50);
+            var x = (short) 10;
+            byte y = 10;
+            byte? z = 10;
+
+            IEnumerable<byte?> expected=new byte?[2];
+            var sut =new Casting();
+            var actual =sut.Explicit(x);
+
+            Assert.IsType<short>(x);
+            Assert.Equal(10, x);
+            Assert.IsType<byte?[]>(actual);
+            Assert.True(expected.Count() == actual.Count());
+            Assert.True(y==actual.ElementAt(0));//positive test
+            Assert.False(z==actual.ElementAt(1));//negative test
+        }
+
+        [Fact]
+        public void TestExplicitOut()
+        {
+            short expectedX=10;
+            byte expectedY=10;
+            byte? expectedZ=null;
+            short x = 10;
+            byte y = 0;
+            byte? z = null;
+
+            Casting sut = new Casting();
+            sut.Explicit(x, out y, out z);
+
+            Assert.Equal(x,expectedX);
+            Assert.Equal(y, expectedY);
+            Assert.Equal(z, expectedZ);
+            Assert.IsType<short>(x);
+            Assert.IsType<byte>(y);
+            Assert.True(z==expectedZ);
+        }
+
+        [Fact]
+        public void TestExplicitRef()
+        {
+            short expectedW = 100;
+            short expectedX = 100;
+            byte expectedY = 10;
+            byte? expectedZ = null;
+            short x = 10;
+            short w = 30;
+
+            Casting sut = new Casting();
+            sut.Explicit(ref w, ref x, out byte y, out byte? z);
+
+            Assert.Equal(w, expectedW);
+            Assert.Equal(x, expectedX);
+            
+        }
+
+        [Fact]
+        public void TestExplicitNoRef()
+        {
+            var expected = 100;
+            short x = 500;
+            short w = 35;
+            Casting sut = new Casting();
+            sut.Explicit(w, x);
+
+            Assert.False(w == expected);
+            Assert.False(x == expected);
+        }
+    }
+}
