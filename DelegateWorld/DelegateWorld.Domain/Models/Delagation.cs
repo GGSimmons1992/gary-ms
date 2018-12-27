5c42fc0ea6a6df00ebe26ed1dac59d838983375e
GGSimmions1992 <gary.simmons@colorado.edu>
2018-12-27 16:55:43 -0500

    Create DelagationWorld

---
+++ DelegateWorld/DelegateWorld.Domain/Models/Delagation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateWorld.Domain.Models
+{
+    public class Delagation
+    {
+        public delegate void NameTag(string n);
+        public delegate string Sharpie(string f,string l);
+        public void TheConference(NameTag nt, Sharpie s, string f,string l)
+        {
+            nt(s(f, l));
+        }
+
+        public void MyConference()
+        {
+            TheConference(
+            (string s) =>{Console.WriteLine(s);}, 
+            (string o, string t) =>
+            {return $"{o} {t}";}, 
+            "fred", 
+            "belotte");
+        }
+
+        public void MyConference2()
+        {
+            TheConference(NameTagMet, SharpieMet, "Fred", "Belotte");
+        }
+
+        private void NameTagMet(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public string SharpieMet(string s,string t)
+        {
+            return $"{s} {t}";
+        }
+    }
+}
---
+++ DelegateWorld/DelegateWorld.Tests/DelegateWorldTests.cs
@@ -0,0 +1,28 @@
+﻿using DelegateWorld.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DelegateWorld.Tests
+{
+    public class DelegateWorldTests
+    {
+        [Fact]
+        private void Sharpie2Test()
+        {
+            var expected = "Gary Simmons";
+            var actual = (new ActionFunc()).Sharpie2("Gary", "Simmons");
+            Assert.Equal(actual,expected);
+        }
+        
+        [Fact]
+        private void SharpieMetTest()
+        {
+            var expected = "Gary Simmons";
+            var actualObject = (new Delagation());
+            var actual=actualObject.SharpieMet("Gary", "Simmons");
+            Assert.Equal(actual, expected);
+        }
+    }
+}
