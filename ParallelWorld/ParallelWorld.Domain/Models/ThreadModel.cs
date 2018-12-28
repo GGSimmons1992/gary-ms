f92f7125714bacb494187de71251ef26f799e221
GGSimmions1992 <gary.simmons@colorado.edu>
2018-12-28 16:43:35 -0500

    Start thread

---
+++ ParallelWorld/ParallelWorld.Domain/Models/ThreadModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ParallelWorld.Domain.Models
+{
+    public class ThreadModel
+    {
+        public string ThreadMaster(string s)
+        {
+            
+
+            var t1 = new Thread(()=> 
+            {
+                Writer('A', 1000);
+            });
+
+            var t2 = new Thread(() => 
+            {
+                Writer('B', 1000);
+            });
+
+            Console.WriteLine(s);
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            //Thread.Sleep(5000);
+            return s;
+        }
+
+        private void Writer(char c, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
---
+++ ParallelWorld/ParallelWorld.Tests/ThreadModelTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ParallelWorld.Domain.Models;
+
+namespace ParallelWorld.Tests
+{
+    public class ThreadModelTest
+    {
+        [Fact]
+        public void ThreadMasterTest()
+        {
+            var sut = new ThreadModel();
+            var expected = "fred";
+            var actual = sut.ThreadMaster(expected);
+
+            Assert.True(expected == actual);
+        }
+    }
+}
