1fc09d3bb5066ef0c392dd3a56c7d8c3e9dbaf7e
GGSimmions1992 <gary.simmons@colorado.edu>
2019-01-10 00:55:19 -0500

    Finish pizza setters

--- PizzaStore/PizzaStore.Data/Helpers/PizzaHelper.cs
+++ PizzaStore/PizzaStore.Data/Helpers/PizzaHelper.cs
@@ -3,6 +3,7 @@ using dom = PizzaStore.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PizzaStore.Data.Helpers
 {
@@ -14,11 +15,32 @@ namespace PizzaStore.Data.Helpers
         {
             return new dom.Pizza()
             {
-                Id = (int) dataPizza.PizzaId
-                ,OrderId= (int) dataPizza.OrderId
-                ,ModifiedDate=dataPizza.ModifiedDate
-                ,crustSize=(int) dataPizza.Size
+                Id = (int)dataPizza.PizzaId
+                , OrderId = (int)dataPizza.OrderId
+                , ModifiedDate = dataPizza.ModifiedDate
+                , crustSize = (int)dataPizza.Size
+                , Toppings = GetIngredientsByPizza(dataPizza)//No Crusts!!!! Toppings Only!!!
+                , price = GetPriceByPizza(dataPizza)
             };
+        }
+
+        public static List<string> GetIngredientsByPizza(Pizza dp)
+        {
+            var toppings = new List<string>();
+
+            var piPairs = _db.PizzaIngredient.Where(pi => pi.PizzaId == dp.PizzaId).ToList();
+
+            foreach (var item in piPairs)
+            {
+                toppings.Add(item.Ingredient.Name);
+            }
+            return toppings;
+        }
+
+        public static double GetPriceByPizza(Pizza dp)
+        {
+            var toppings=GetIngredientsByPizza(dp);
+            return (double)((0.75*dp.Size) + (0.50*toppings.Count));
         }
 
     }
