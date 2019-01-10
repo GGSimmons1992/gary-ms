70c064b89cd69df83af1497ef4fabd013d9e04f0
GGSimmions1992 <gary.simmons@colorado.edu>
2019-01-09 23:12:32 -0500

    Finish DOMLocation in LocationHelper

--- PizzaStore/PizzaStore.Data/Helpers/LocationHelper.cs
+++ PizzaStore/PizzaStore.Data/Helpers/LocationHelper.cs
@@ -17,6 +17,10 @@ namespace PizzaStore.Data.Helpers
             {
                 Id=dataLocation.LocationId
                 ,ModifiedDate=dataLocation.ModifiedDate
+                ,Inventory=GetInventoryByLocation(dataLocation)
+                ,userlist=GetUsersByLocation(dataLocation)
+                ,History=GetOrdersByLocation(dataLocation)
+                ,Ledger=GetSalesByLocation(dataLocation)
             };
         }
 
@@ -29,6 +33,60 @@ namespace PizzaStore.Data.Helpers
 
 
             return domStore;
+        }
+
+        public static Dictionary<string, int> GetInventoryByLocation(Location dl)
+        {
+            var inventory = new Dictionary<string, int>();
+            
+            var DesiredLIPairs = _db.LocationIngredient.Where(li => li.LocationId == dl.LocationId).ToList();
+
+            foreach (var item in DesiredLIPairs)
+            {
+                inventory.Add(item.Ingredient.Name, (int)item.InventoryAmount);
+            }
+
+            return inventory;
+        }
+
+        public static List<dom.User> GetUsersByLocation(Location dl)
+        {
+            var userlist = new List<dom.User>();
+
+            var DesiredLUPairs = _db.LocationUser.Where(lu => lu.LocationId == dl.LocationId).ToList();
+
+            foreach (var item in DesiredLUPairs)
+            {
+                userlist.Add(UserHelper.DOMUser(item.User));
+            }
+
+            return userlist;
+        }
+
+        public static List<dom.Order> GetOrdersByLocation(Location dl)
+        {
+            var orders = new List<dom.Order>();
+
+            var desiredOrders = _db.Order.Where(r=>r.StoreId==dl.LocationId).ToList();
+
+            foreach (var item in desiredOrders)
+            {
+                orders.Add(OrderHelper.DOMOrder(item));
+            }
+
+            return orders;
+
+        }
+
+        public static double GetSalesByLocation(Location dl)
+        {
+            var OrderList=GetOrdersByLocation(dl);
+            double sales = 0;
+            foreach (var item in OrderList)
+            {
+                sales += item.Cost();
+            }
+            return sales;
         }
 
     }
