0c1b9b92dc9c59f553309de62fb069b3ba5acc0b
GGSimmions1992 <gary.simmons@colorado.edu>
2019-03-01 11:50:20 -0500

    play with routing

--- HeroWorld/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ HeroWorld/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
-@Injectable({
-  providedIn: 'root',
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+
 })
-export class MessageService {
-  messages: string[] = [];
-
-  add(message: string) {
-    this.messages.push(message);
-  }
-
-  clear() {
-    this.messages = [];
-  }
-}
+export class AppRoutingModule { }
