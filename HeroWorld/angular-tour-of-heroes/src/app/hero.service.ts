47da762e5f26893e8e2f95a1742cc222375faf12
GGSimmions1992 <gary.simmons@colorado.edu>
2019-03-03 15:17:50 -0500

    Add Delete method to heroes service

--- HeroWorld/angular-tour-of-heroes/src/app/hero.service.ts
+++ HeroWorld/angular-tour-of-heroes/src/app/hero.service.ts
@@ -47,6 +47,7 @@ export class HeroService {
     tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
     catchError(this.handleError<Hero>('addHero'))
   );
+
 }
 
   /** PUT: update the hero on the server */
@@ -54,6 +55,17 @@ export class HeroService {
   return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
     tap(_ => this.log(`updated hero id=${hero.id}`)),
     catchError(this.handleError<any>('updateHero'))
+  );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+  const id = typeof hero === 'number' ? hero : hero.id;
+  const url = `${this.heroesUrl}/${id}`;
+
+  return this.http.delete<Hero>(url, httpOptions).pipe(
+    tap(_ => this.log(`deleted hero id=${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
   );
   }
 
